c16081d168c772895c69ae9318e9182084f35418
wkava57 <wabutrune.p@hotmail.com>
2022-03-24 08:57:56 +0100

    init

---
+++ app.js
@@ -0,0 +1,89 @@
+// function opacityOne(param){
+//     param.style.opacity="1";
+// }
+//     let sac = document.getElementById('sac');
+//     sac.addEventListener("click", function(){
+//         opacityOne(sac);
+//     });
+
+
+// let dix = 0
+// while(dix<250){
+//     console
+// }
+
+// let spam = 0;
+// while(spam < 10){
+//     console.log("hello spam :  "+spam);
+//     spam++;
+// };
+// pour envoyer hello spam dix foix
+
+// do {
+
+//     var nombre = prompt("Veuillez saisir un nombre entier");
+// }
+//     while(nombre == "" || nombre == null || !parseInt(nombre))   
+//     alert("votre nombre est")
+
+    // parseInt = renvoie vrai si le paramètre est une valeur numérique entier
+    // parseFloat = renvoie vrai si le paramètre est une valeur numérique décimale
+    // isNan = renvoie vrai si le paramètre est une valeur qui n'est pas numérique
+    // Number.Integer = Non car ne distingue paas les valeurs et seulement le type (et comme prompt renvoie une string (chaine de caractère))
+    // j'aurais toujours faux)
+
+
+// for(let i = 0; i<7; i++){
+//     console.log("Milan est le meilleur des profs");
+    
+// }
+
+// for(let i = 0; i <15; i++){
+//     console.log()
+// }
+// let listeClasse = ["Younes", "Xavier", "Nelly", "Théo", "Bouba","kava"];
+// listeClasse.forEach(element => console.log(element));
+
+// for(let i = 0; i < listeClasse.length; i++){
+//     console.log(listeClasse[i])
+// }
+// let compteur = "0";
+// while(compteur<144){
+//     console.log("mot");
+//     word++;
+// }
+// let confirm = document.getElementById('sac');
+//     .addEventListener("click", function(){
+//         opacityOne();
+//     });
+// for(let word = 0; word<144; word++){
+//     console.log("hello :"+word);
+// }
+// function confirmation
+//     console.log()
+
+// do{
+//     var confirm = confirm("Veuiller saisir OK");
+//     }while (confirmation == false);
+
+// let infosPersos = [46, "kava", "wabutrune","Ambérieu"];
+
+// alert("hello, je m'appelle "+infosPersos[1]+" "+infosPersos[2]+". J'habite "+infosPersos[3]+",et j'ai "+infosPersos[0]+"ans")
+
+let tabMot = [
+    ["hello", "je", "j'ai", "au revoir"],
+    ["ans", "dans", "soif", "faim", "café"],
+    ["toujours", "jamais", "matin", "soir"],
+    ["parfois", "élève", "cours", "développement web"],
+    ["ordinateur", "film", "nuit", "sommeil"],
+    ["clavier", "téléphone", "voiture", "trajet"]
+];
+// alert(`hello ${tabMot[0][0]} ${tabMot[0][2]} "un" ${tabMot[3][2]} "de" ${tabMot[3][3]} "ce" ${tabMot[2][3]}`)
+
+// ""+tabMot[0][1]+"te"+tabMot[5][1]+"ce"+tabMot[2][3]+"après le"+tabMot[4][1]+""
+for(let a = 0; a < tabMot.length; a++){
+    for(let b = 0; b < tabMot[0].length; b++){
+        console.log(tabMot[0][0].length);
+    }
+// }4
+// console.log(tabMot[0].length)
